1d235880039bc25a628c935f313bc63d0b62d666
pharmakarios1 <akhigbeblessed@gmail.com>
2023-12-05 12:30:17 +0100

    new fixes on babel

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
+#!/usr/bin/env node
 const server = require("express");
 const dotenv = require("dotenv").config();
 const app = server();
 
 
 
-
+const PORT = process.env.PORT || 3000;
 app.use(server.json());
+
 app.get("/", (req, res) => {
     res.send("Hello World");
     });
 
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 })
