cac7c590d02902de91720d9b16d71cb307d41898
pharmakarios1 <akhigbeblessed@gmail.com>
2023-12-09 04:13:59 +0100

    [Chore]: Use fixes

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 const server = require("express");
+const router = require('./routes/index')
 const dotenv = require("dotenv").config();
 const app = server();
-
-
-
 const PORT = process.env.PORT || 3000;
-app.use(server.json());
 
-app.get("/", (req, res) => {
+
+
+
+
+app.use(server.json());
+app.use("/", router)
+
+app.get("/hello", (req, res) => {
     res.send("Hello World");
     });
 
